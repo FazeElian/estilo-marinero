80b1c3d738901ef9b9544dd13f06b78ad6eea67f
FazeElian <elian.ibarra@soy.sena.edu.co>
2024-02-06 21:59:08 -0500

    Creation of Products List and search function

---
+++ src/assets/js/HomeProductsList.js
@@ -0,0 +1,88 @@
+const homeProductsList = [
+    {
+        id: 1,
+        name: "Producto 1",
+        category: "Categoría 1",
+        imgSrc: require("../img/products/productImage1.jpg"),
+        imgAlt: "Producto 1"
+    },
+    {
+        id: 2,
+        name: "Producto 2",
+        category: "Categoría 2",
+        imgSrc: require("../img/products/productImage1.jpg"),
+        imgAlt: "Producto 2"
+    },
+    {
+        id: 3,
+        name: "Producto 3",
+        category: "Categoría 1",
+        imgSrc: require("../img/products/productImage1.jpg"),
+        imgAlt: "Producto 3"
+    },
+    {
+        id: 4,
+        name: "Producto 4",
+        category: "Categoría 3",
+        imgSrc: require("../img/products/productImage1.jpg"),
+        imgAlt: "Producto 4"
+    },
+    {
+        id: 5,
+        name: "Producto 5",
+        category: "Categoría 4",
+        imgSrc: require("../img/products/productImage1.jpg"),
+        imgAlt: "Producto 5"
+    },
+    {
+        id: 6,
+        name: "Producto 6",
+        category: "Categoría 2",
+        imgSrc: require("../img/products/productImage1.jpg"),
+        imgAlt: "Producto 6"
+    },
+    {
+        id: 7,
+        name: "Producto 7",
+        category: "Categoría 1",
+        imgSrc: require("../img/products/productImage1.jpg"),
+        imgAlt: "Producto 7"
+    },
+    {
+        id: 8,
+        name: "Producto 8",
+        category: "Categoría 4",
+        imgSrc: require("../img/products/productImage1.jpg"),
+        imgAlt: "Producto 8"
+    },
+    {
+        id: 9,
+        name: "Producto 9",
+        category: "Categoría 8",
+        imgSrc: require("../img/products/productImage1.jpg"),
+        imgAlt: "Producto 9"
+    },
+    {
+        id: 10,
+        name: "Producto 10",
+        category: "Categoría 1",
+        imgSrc: require("../img/products/productImage1.jpg"),
+        imgAlt: "Producto 10"
+    },
+    {
+        id: 11,
+        name: "Producto 11",
+        category: "Categoría 2",
+        imgSrc: require("../img/products/productImage1.jpg"),
+        imgAlt: "Producto 11"
+    },
+    {
+        id: 12,
+        name: "Producto 12",
+        category: "Categoría 1",
+        imgSrc: require("../img/products/productImage1.jpg"),
+        imgAlt: "Producto 12"
+    }
+];
+
+export default homeProductsList;
--- src/views/HomeView.jsx
+++ src/views/HomeView.jsx
@@ -2,13 +2,55 @@
 import { Header } from "../components/Header";
 
 // Styles
-import "../assets/css/views/HomeView.css";
+import "../assets/css/views/HomeView.css"; // Home View
+import "../assets/css/components/ProductCard.css"; // Product Card
+import "../assets/css/components/ProductsList.css"; // Products List
+
+// useState, useEffect hooks
+import { useState, useEffect } from "react";
+
+// Routes navigation
+import { Link } from "react-router-dom";
+
+// Products List
+import homeProductsList from "../assets/js/HomeProductsList";
 
 // Images - Icons
     // Search Icon
     import SearchIcon from "../assets/img/icons/Search.png";
 
 const HomeView = () => {
+    const [ searchProduct, setSearchProduct ] = useState("");
+    const [ products, setProducts ] = useState([]);
+
+    // Función para traer datos de Arreglos JSON    
+    const showData = async () => {
+        setProducts(homeProductsList);
+    }
+
+    // Función para eliminar acentos de una cadena
+    const removeAccents = (str) => {
+        return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+    };
+
+    // Método de filtrado
+    const searchResult = !searchProduct
+        ? products
+        : products.filter(
+            (dataProduct) =>
+            removeAccents(dataProduct.name?.toLowerCase()).includes(removeAccents(searchProduct.toLowerCase())) ||
+            removeAccents(dataProduct.category?.toLowerCase()).includes(removeAccents(searchProduct.toLowerCase()))
+        );
+
+    // Función de búsqueda
+    const productsSearcher = (e) => {
+        setSearchProduct(e.target.value);
+    };
+
+    useEffect ( () => {
+        showData()
+    }, [])
+
     return(
         <>
             <Header />
@@ -29,9 +71,32 @@ const HomeView = () => {
                         <button className="btn-searcher">
                             <img src={SearchIcon} alt="Icono Búsqueda" />
                         </button>
-                        <input type="text" className="searcher" name="" id="" placeholder="Buscar Producto" />
+                        <input 
+                            type="text" 
+                            className="searcher" 
+                            name="" 
+                            id="" 
+                            placeholder="Buscar Producto"
+                            value={searchProduct}
+                            onChange={productsSearcher}
+                        />
                     </div>
                 </div>
+            </div>
+
+            <div className="cont-products-list">
+                {searchResult.map(product =>(
+                    <div className="cont-product-card" key={product.id}>
+                        <div className="cont-img-prod-card">
+                            <Link to="/"><img src={product.imgSrc} alt={product.imgAlt} /></Link>
+                        </div>
+
+                        <div className="cont-content-prod-card">
+                            <h2><Link to="/">{product.name}</Link></h2>
+                            <h3><Link to="/categories/1"><b>Categoría:</b></Link> {product.category}</h3>
+                        </div>
+                    </div>
+                ))}
             </div>
         </>
     )
