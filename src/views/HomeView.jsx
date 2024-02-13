7ea804d675312a21aff6232cb3119e04b85d8659
FazeElian <elian.ibarra@soy.sena.edu.co>
2024-02-12 22:55:20 -0500

    Creation of Categories Section in Home View

--- src/views/HomeView.jsx
+++ src/views/HomeView.jsx
@@ -5,6 +5,7 @@ import { Header } from "../components/Header";
 import "../assets/css/views/HomeView.css"; // Home View
 import "../assets/css/components/ProductCard.css"; // Product Card
 import "../assets/css/components/ProductsList.css"; // Products List
+import "../assets/css/components/ProductsCategoriesList.css"; // Products Categories List
 
 // useState, useEffect hooks
 import { useState, useEffect } from "react";
@@ -18,7 +19,13 @@ import homeProductsList from "../assets/js/HomeProductsList";
 // Images - Icons
     // Search Icon
     import SearchIcon from "../assets/img/icons/Search.png";
-import { SeeAllButton } from "../components/SeeAllButton";
+
+    // Se All Button
+    import { SeeAllButton } from "../components/SeeAllButton";
+
+    // Products Categories
+        // Example:
+        import ProductCategorieExampleImage from "../assets/img/products/productImage1.jpg";
 
 const HomeView = () => {
     const [ searchProduct, setSearchProduct ] = useState("");
@@ -51,6 +58,52 @@ const HomeView = () => {
     useEffect ( () => {
         showData()
     }, [])
+
+    // Products Categories
+    const productsCategories = [
+        {
+            id: 1,
+            name: "Categoría 1",
+            link: "/categorie-1",
+            imgSrc: ProductCategorieExampleImage,
+            imgAlt: "Categoría 1",
+        },
+        {
+            id: 2,
+            name: "Categoría 2",
+            link: "/categorie-2",
+            imgSrc: ProductCategorieExampleImage,
+            imgAlt: "Categoría 2",
+        },
+        {
+            id: 3,
+            name: "Categoría 3",
+            link: "/categorie-3",
+            imgSrc: ProductCategorieExampleImage,
+            imgAlt: "Categoría 3",
+        },
+        {
+            id: 4,
+            name: "Categoría 4",
+            link: "/categorie-4",
+            imgSrc: ProductCategorieExampleImage,
+            imgAlt: "Categoría 4",
+        },
+        {
+            id: 5,
+            name: "Categoría 5",
+            link: "/categorie-5",
+            imgSrc: ProductCategorieExampleImage,
+            imgAlt: "Categoría 5",
+        },
+        {
+            id: 6,
+            name: "Categoría 6",
+            link: "/categorie-6",
+            imgSrc: ProductCategorieExampleImage,
+            imgAlt: "Categoría 6",
+        }
+    ];
 
     return(
         <>
@@ -99,11 +152,39 @@ const HomeView = () => {
                     </div>
                 ))}
             </div>
-
+            
             {/* See All Products Button Component */}
             <SeeAllButton
                 link="/products/"
                 text="Ver todos los productos" 
+            />
+
+            <div className="content-page">
+                <div className="cont-products-categories">
+                    <div className="cont-title-categories-home">
+                        Categorías de Productos
+                    </div>
+                    <div className="cont-list-products-categories">
+                        {productsCategories.map (productsCategorie => ((
+                            <div className="products-categorie" key={productsCategorie.id}>
+                                <Link to={productsCategorie.link}>
+                                    <div className="img-products-categorie">
+                                        <img src={productsCategorie.imgSrc} alt={productsCategorie.imgAlt} />
+                                    </div>
+                                    <div className="content-products-categorie">
+                                        <h2>{productsCategorie.name}</h2>
+                                    </div>
+                                </Link>
+                            </div>
+                        )))}                
+                    </div>
+                </div>
+            </div>
+
+            {/* See All Categories Button Component */}
+            <SeeAllButton
+                link="/categories/"
+                text="Ver todas las Categorías" 
             />
         </>
     )
